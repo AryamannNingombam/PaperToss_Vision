b3005d2f048bc69565a7b0f3b36a6f5da299350e
ApoorvShah111 <apoorvshah111@gmail.com>
2021-10-28 19:18:55 +0530

    FanAddition

--- Assets/Scripts/DetectSwipe.cs
+++ Assets/Scripts/DetectSwipe.cs
@@ -9,10 +9,22 @@ public class DetectSwipe : MonoBehaviour,ISwipeHandler
     [SerializeField]
     Rigidbody ball;
 
+
+    [SerializeField]
+    public GameObject Fan;
+    [SerializeField]
+    public Transform FanT;
+    Vector3 Pos1 = new Vector3(-2.41f, 2.66f, 7.69f);
+    Vector3 Pos2 = new Vector3(2.01f, 2.66f, 7.69f);
+    int p = 0;
+
+
     int time = 0;
     bool start = false;
     bool startTimer = false;
     float timerValue = 75;
+    //[SerializeField]
+    //FanWind wind;
 
 
     void ISwipeHandler.OnSwipeCanceled(SwipeEventData eventData)
@@ -57,7 +69,7 @@ public class DetectSwipe : MonoBehaviour,ISwipeHandler
         
         print("Swipe!");
 
-
+        addwind();
         //Instantiate(ball, new Vector3(0, 3.08f, 2.07f), Quaternion.identity);
 
     }
@@ -67,10 +79,45 @@ public class DetectSwipe : MonoBehaviour,ISwipeHandler
         
     }
 
+    public void changepos()
+    {
+
+        //changing the position of the fan
+        if (p == 0)
+        {
+            FanT.localPosition = Pos2;
+            p = 1;
+        }
+        else
+        {
+            FanT.localPosition = Pos1;
+            p = 0;
+        }
+
+    }
+
+
+    public void addwind()
+    {
+        //Adding veloity
+        if (p == 0)
+        {
+            print("entering1");
+            ball.AddForce(new Vector3(100, 0, 0));
+        }
+        else
+        {
+            print("entering2");
+            ball.AddForce(-100, 0, 0);
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         JMRInputManager.Instance.AddGlobalListener(gameObject);
+        FanT.localPosition = Pos1;
     }
 
     // Update is called once per frame
@@ -90,8 +137,10 @@ public class DetectSwipe : MonoBehaviour,ISwipeHandler
             {
                 print("Times up!");
                 ball = Instantiate(ball, new Vector3(0, 3.08f, 2.07f), Quaternion.identity);
+                //wind.PaperR=Instantiate(wind.PaperR, new Vector3(0, 3.08f, 2.07f), Quaternion.identity);
                 timerValue = 75;
                 startTimer = false;
+                changepos();
             }
             
         }
